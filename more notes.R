5ff3edf4a94af1752312fce9e073e548f84c98ec
Akshar Gandhi <akshar.gandhi99@gmail.com>
2019-01-12 20:14:12 +0530

    Adding files

---
+++ more notes.R
@@ -0,0 +1,22 @@
+#In markdown
+#header
+#To create an R chunk, use ctrl+Alt+I
+
+#To not show the the code while presenting, use echo = FALSE
+
+#```{r, echo = FALSE}
+#plot(cars)
+#```
+
+#Add a summary to each R code chunk
+
+#```{r cars - summary }
+#summary(cars)
+#```
+summary(pressure)
+
+#For markdown, use 
+#github_document
+#word_document
+#pdf_document
+#html_document
